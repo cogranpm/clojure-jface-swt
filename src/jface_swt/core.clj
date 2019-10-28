cc42d430b60270017f6bb0b45517897317b33034
paul martin <mussym@live.com>
2019-10-27 23:54:29 -0500

    window stuff

--- src/jface_swt/core.clj
+++ src/jface_swt/core.clj
@@ -3,17 +3,47 @@
   (:import (org.eclipse.jface.layout GridDataFactory))
   (:import (org.eclipse.jface.window ApplicationWindow))
   (:import (org.eclipse.swt.graphics Point))
+  (:import (org.eclipse.swt SWT))
+  (:import (org.eclipse.jface.action ToolBarManager StatusLineManager MenuManager Separator))
   (:gen-class))
+
+
+(def menu-manager
+  (new MenuManager "menu"))
 
 (def my-app-window
   (proxy
       [ApplicationWindow]
       [nil]
+
+    
     (createContents [parent]
       (println "create contents called"))
 
     (getInitialSize []
       (new Point 500 500))
+
+    (configureShell [newShell]
+      (proxy-super configureShell newShell)
+      (. newShell (setText "Kernai"))
+      ;;should be setting images here on the shell
+      )
+
+    (createMenuManager []
+      menu-manager)
+      
+
+    (createStatusLineManager []
+      (new StatusLineManager))
+
+    (createToolBarManager [style]
+      (new ToolBarManager style))
+
+    (close []
+      (proxy-super close)
+      (println "closing application"))
+
+    
     ))
 
     
@@ -41,6 +71,9 @@
   (println "Hello, World!")
   ;;(gui-main)
   (. my-app-window setBlockOnOpen true)
+  (. my-app-window addMenuBar)
+  (. my-app-window addStatusLine)
+  (. my-app-window addToolBar (bit-or (. SWT FLAT) (. SWT WRAP)))
   (. my-app-window open)
   (. default-display (dispose))
   )
