60ff4f18074ae44ae01e11483232012e88d51b7d
paul martin <mussym@live.com>
2020-11-18 00:13:50 -0600

    we are back using maven for the dependencies

--- project.clj
+++ project.clj
@@ -2,9 +2,13 @@
                               ["os.arch" "os.name"])
       platform (apply format "%s (%s)" (vals properties))
       swt (case platform
-            "amd64 (Windows 10)" "lib/org.eclipse.swt.win32.win32.x86_64_3.114.0.v20200304-0601.jar"
-            "amd64 (Linux)"      "lib/org.eclipse.swt.gtk.linux.x86_64_3.112.0.v20190904-0609.jar"
-            "x86_64 (Mac OS X)" "lib/org.eclipse.swt.cocoa.macosx.x86_64_3.110.0.v20190305-0602.jar" )
+            ;;"amd64 (Windows 10)" "lib/org.eclipse.swt.win32.win32.x86_64_3.114.0.v20200304-0601.jar"
+            ;;"amd64 (Linux)"      "lib/org.eclipse.swt.gtk.linux.x86_64_3.112.0.v20190904-0609.jar"
+            ;;"x86_64 (Mac OS X)" "lib/org.eclipse.swt.cocoa.macosx.x86_64_3.110.0.v20190305-0602.jar" )
+            "amd64 (Windows 10)" org.eclipse.platform/org.eclipse.swt.win32.win32.x86_64 "3.110.0"
+            "amd64 (Linux)"      org.eclipse.platform/org.eclipse.swt.gtk.linux.x86_64 "3.110.0"
+            "x86_64 (Mac OS X)" org.eclipse.platform/org.eclipse.swt.cocoa.macosx.x86_64 "3.110.0" )
+
       jvmOpts (case platform
                 "amd64 (Windows 10)" (list) 
                 "amd64 (Linux)" (list)
@@ -20,25 +24,118 @@
 
 
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojars.nakkaya/miglayout "3.7.3.1"]
-                 ;;note, there are dependencies for all the eclipse stuff in maven
-                 ;;but it seems to be a mess
-                 ;;for now these deps will be managed manually in lib directory via the resource-paths attribute below
-                 ;;this means that they won't be packaged quite right in uberjar so created a shell script runner.sh to set the classpath
-                 ;;still uses the uberjar underneath
 
-                 ;;here are examples of the maven deps for eclipse stuff
-                 ;;[org.eclipse/swt-gtk-linux-x86_64 "3.5.2"]
-                 ;;[org.eclipse.swt/org.eclipse.swt.cocoa.macosx.x86_64 "4.3"]
-                 ;;[org.eclipse/jface "3.3.0-I20070606-0010"]
-                 ;;[org.eclipse.core/org.eclipse.core.runtime "3.7.0"]
-                 ;;[org.eclipse.platform/org.eclipse.swt "3.112.0"]
-                 ;;causes a big fat crash
-                 ;;[org.eclipse.platform/org.eclipse.jface "3.17.0" :exclusions [org.eclipse.platform/org.eclipse.equinox.common org.eclipse.platform/org.eclipse.core.commands org.eclipse.platform/org.eclipse.swt]]
-                 ;;[org.eclipse.platform/org.eclipse.text "3.9.0"]
-                 ;;[org.eclipse.platform/org.eclipse.core.jobs "3.10.500"]
-                 ;;[org.eclipse.platform/org.eclipse.jface.databinding "1.9.100"]
-                 ;;[org.eclipse.platform/org.eclipse.core.databinding.property "1.7.100" :exclusions [org.eclipse.platform/org.eclipse.equinox.common org.eclipse.platform/org.eclipse.core.databinding.observable] ]
+                 [org.clojars.nakkaya/miglayout "3.7.3.1"]
+
+                 [~swt]
+
+                 [org.eclipse.platform/org.eclipse.swt "3.114.100"]
+
+                 [org.eclipse.platform/org.eclipse.core.runtime "3.19.0"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   org.eclipse.platform/org.eclipse.equinox.registry
+                   org.eclipse.platform/org.eclipse.osgi
+                   org.eclipse.platform/org.eclipse.equinox.app
+                   org.eclipse.platform/org.eclipse.equinox.preferences
+                   org.eclipse.platform/org.eclipse.core.contenttype
+                   org.eclipse.platform/org.eclipse.core.jobs
+                   org.eclipse.platform/org.eclipse.core.runtime
+                   org.eclipse.platform/org.eclipse.core.commands
+
+                   ]
+                  ]
+
+                 [org.eclipse.platform/org.eclipse.core.commands "3.9.700"
+                  :exclusions
+                  [org.eclipse.platform/org.eclipse.equinox.common]]
+
+                 [org.eclipse.platform/org.eclipse.equinox.common "3.12.0"]
+
+                 [com.ibm.icu/icu4j "67.1"]
+
+                 [org.eclipse.platform/org.eclipse.core.jobs "3.10.800"
+                  :exclusions [org.eclipse.platform/org.eclipse.equinox.common]]
+
+                 [org.eclipse.platform/org.eclipse.osgi "3.15.300"]
+
+                 [org.eclipse.platform/org.eclipse.core.databinding.beans "1.6.100"
+                  :exclusions
+                  [org.eclipse.platform/org.eclipse.core.databinding.observable
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   org.eclipse.platform/org.eclipse.core.databinding.property]]
+
+                 [org.eclipse.platform/org.eclipse.core.databinding.observable "1.9.0"
+                  :exclusions
+                  [org.eclipse.platform/org.eclipse.equinox.common]]
+
+                 [org.eclipse.platform/org.eclipse.core.databinding "1.9.0"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   org.eclipse.platform/org.eclipse.core.databinding.observable
+                   org.eclipse.platform/org.eclipse.core.databinding.property
+                   ]]
+
+                 [org.eclipse.platform/org.eclipse.core.databinding.property "1.8.0"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.core.databinding.observable
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   ]]
+
+                 [org.eclipse.platform/org.eclipse.jface.databinding "1.11.100"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.jface
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   org.eclipse.platform/org.eclipse.core.databinding 
+                   org.eclipse.platform/org.eclipse.core.databinding.observable
+                   org.eclipse.platform/org.eclipse.core.databinding.property
+                   org.eclipse.platform/org.eclipse.swt]
+                  ]
+
+                 [org.eclipse.platform/org.eclipse.jface "3.20.0"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.swt
+                   org.eclipse.platform/org.eclipse.core.commands
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   ]]
+
+                 [org.eclipse.platform/org.eclipse.text "3.10.200"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   org.eclipse.platform/org.eclipse.equinox.registry
+                   org.eclipse.platform/org.eclipse.osgi
+                   org.eclipse.platform/org.eclipse.equinox.app
+                   org.eclipse.platform/org.eclipse.equinox.preferences
+                   org.eclipse.platform/org.eclipse.core.contenttype
+                   org.eclipse.platform/org.eclipse.core.jobs
+                   org.eclipse.platform/org.eclipse.core.runtime
+                   org.eclipse.platform/org.eclipse.core.commands
+                   ]
+                  ]
+
+                 [org.eclipse.platform/org.eclipse.jface.text "3.16.300"
+                  :exclusions
+                  [
+                   org.eclipse.platform/org.eclipse.swt
+                   org.eclipse.platform/org.eclipse.jface
+                   org.eclipse.platform/org.eclipse.text
+                   org.eclipse.platform/org.eclipse.equinox.common
+                   org.eclipse.platform/org.eclipse.equinox.registry
+                   org.eclipse.platform/org.eclipse.osgi
+                   org.eclipse.platform/org.eclipse.equinox.app
+                   org.eclipse.platform/org.eclipse.equinox.preferences
+                   org.eclipse.platform/org.eclipse.core.contenttype
+                   org.eclipse.platform/org.eclipse.core.jobs
+                   org.eclipse.platform/org.eclipse.core.runtime
+                   org.eclipse.platform/org.eclipse.core.commands
+                   ]
+                  ]
                  ]
   :repositories [["java.net" "https://download.java.net/maven/2"]
                   ["sonatype" {:url "https://oss.sonatype.org/content/repositories/releases"
@@ -57,21 +154,21 @@
                               ;; You can also apply them to releases only:
                                :releases {:checksum :fail :update :always}}]]
   :resource-paths ["resources/images"
-                   "lib/com.ibm.icu_64.2.0.v20190507-1337.jar"
-                   "lib/org.eclipse.core.commands_3.9.700.v20191217-1850.jar"
-                   "lib/org.eclipse.core.databinding.beans_1.6.100.v20191217-1850.jar"
-                   "lib/org.eclipse.core.databinding.observable_1.9.0.v20200205-2119.jar"
-                   "lib/org.eclipse.core.databinding.property_1.8.0.v20200124-0715.jar"
-                   "lib/org.eclipse.core.databinding_1.8.0.v20200205-2008.jar"
-                   "lib/org.eclipse.core.runtime_3.17.100.v20200203-0917.jar"
-                   "lib/org.eclipse.jface.databinding_1.11.0.v20200205-2119.jar"
-                   "lib/org.eclipse.jface.text_3.16.200.v20200218-0828.jar"
-                   "lib/org.eclipse.jface_3.19.0.v20200218-1607.jar"
-                   "lib/org.eclipse.osgi_3.15.200.v20200214-1600.jar"
-                   ~swt
-                   "lib/org.eclipse.swt_3.114.0.v20200304-0601.jar"
-                   "lib/org.eclipse.text_3.10.100.v20200217-1239.jar"
-                   "lib/org.eclipse.equinox.common_3.11.0.v20200206-0817.jar"
+                   ;;"lib/com.ibm.icu_64.2.0.v20190507-1337.jar"
+                   ;;"lib/org.eclipse.core.commands_3.9.700.v20191217-1850.jar"
+                   ;;"lib/org.eclipse.core.databinding.beans_1.6.100.v20191217-1850.jar"
+                   ;;"lib/org.eclipse.core.databinding.observable_1.9.0.v20200205-2119.jar"
+                   ;;"lib/org.eclipse.core.databinding.property_1.8.0.v20200124-0715.jar"
+                   ;;"lib/org.eclipse.core.databinding_1.8.0.v20200205-2008.jar"
+                   ;;"lib/org.eclipse.core.runtime_3.17.100.v20200203-0917.jar"
+                   ;;"lib/org.eclipse.jface.databinding_1.11.0.v20200205-2119.jar"
+                   ;;"lib/org.eclipse.jface.text_3.16.200.v20200218-0828.jar"
+                   ;;"lib/org.eclipse.jface_3.19.0.v20200218-1607.jar"
+                   ;;"lib/org.eclipse.osgi_3.15.200.v20200214-1600.jar"
+                   ;;~swt
+                   ;;"lib/org.eclipse.swt_3.114.0.v20200304-0601.jar"
+                   ;;"lib/org.eclipse.text_3.10.100.v20200217-1239.jar"
+                   ;;"lib/org.eclipse.equinox.common_3.11.0.v20200206-0817.jar"
                    ]
   :main ^:skip-aot jface-swt.core
   :target-path "target/%s"
